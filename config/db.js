3657ed70dab6806758c8a3cab3b8aaf30cc0def3
dhaydt <1992dayat@gmail.com>
2021-09-18 18:14:53 +0700

    fix twit and fb card

--- config/db.js
+++ config/db.js
@@ -3,7 +3,8 @@ import mysql from "mysql2";
 // create the connection to database
 const db = mysql.createConnection({
   host: "localhost",
-  // user: "root",  password: "11111",
+  // user: "root",
+  // password: "11111",
   user: "admin",
   password: "",
   database: "kamusdb",
